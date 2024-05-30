5994e62d41a57a440334b8bf3fd16419c72cb516
carl0sfelipe <carlos.felipe@hotmail.com.br>
2024-05-30 12:08:36 -0300

    new

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private loginUrl = 'https://dev230375.service-now.com/api/x_786590_basic_u_0/user/login';
-  private registerUrl = 'https://dev230375.service-now.com/api/x_786590_basic_u_0/user/register'; // Adicione a URL correta aqui
+  private loginUrl = 'https://dev273264.service-now.com/api/x_786590_basic_u_0/user/login';
+  private registerUrl = 'https://dev273264.service-now.com/api/x_786590_basic_u_0/user/register'; // Adicione a URL correta aqui
 
   constructor(private http: HttpClient) {}
 
@@ -15,7 +15,7 @@ export class AuthService {
     const headers = new HttpHeaders({
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': 'Basic ' + btoa('admin:admin')
+      'Authorization': 'Basic ' + btoa('admin:79moBB!uEnV^')
     });
     const body = JSON.stringify({ email: email, password: password });
     return this.http.post(this.loginUrl, body, { headers });
@@ -24,7 +24,8 @@ export class AuthService {
   register(name: string, email: string, password: string): Observable<any> {
     const headers = new HttpHeaders({
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + btoa('admin:79moBB!uEnV^')
     });
     const body = JSON.stringify({ name: name, email: email, password: password });
     return this.http.post(this.registerUrl, body, { headers });
